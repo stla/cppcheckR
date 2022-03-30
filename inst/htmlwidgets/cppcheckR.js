f2fb66fdf974da65985ee98320bac9e8d0f020b6
stla <laurent_step@yahoo.fr>
2022-03-30 04:42:09 +0200

    cwe links

--- inst/htmlwidgets/cppcheckR.js
+++ inst/htmlwidgets/cppcheckR.js
@@ -30,6 +30,7 @@ HTMLWidgets.widget({
       falseColor: "#ff8080",
       nullColor: "cornflowerblue"
     };
+    const xpath = "//span[text()='\"@_cwe\":']";
 
     return {
       renderValue: function (x) {
@@ -43,7 +44,8 @@ HTMLWidgets.widget({
         }
         var pre = document.createElement("PRE");
         pre.innerHTML = jsonFormatHighlight(
-          jUnescape(JSON.stringify(json, replacer, 2)), customColorOptions
+          jUnescape(JSON.stringify(json, replacer, 2)),
+          customColorOptions
         );
         pre.style.whiteSpace = "pre-wrap";
         el.style.color = "#E76900";
@@ -55,6 +57,33 @@ HTMLWidgets.widget({
 
         // TODO: code to render the widget, e.g.
         el.appendChild(pre);
+
+        var spans = document.evaluate(
+          xpath,
+          document,
+          null,
+          XPathResult.ORDERED_NODE_ITERATOR_TYPE,
+          null
+        );
+        var result = spans.iterateNext();
+        var results = [];
+        while (result) {
+          results.push(result);
+          result = spans.iterateNext();
+        }
+        for (var i = 0; i < results.length; i++) {
+          var span = results[i].nextElementSibling;
+          var code = parseInt(JSON.parse(span.innerText));
+          span.innerText = "";
+          var a = document.createElement("A");
+          a.style.color = "aquamarine";
+          a.setAttribute(
+            "href",
+            "https://cwe.mitre.org/data/definitions/" + code + ".html"
+          );
+          a.appendChild(document.createTextNode(code));
+          span.appendChild(a);
+        }
       },
 
       resize: function (width, height) {
